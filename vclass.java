9e9852cb50fb22d3719218d00bba6afe29f841ad
Ragul1996 <sacariyaragul@gmail.com>
2022-07-18 12:39:37 +0530

    project firstpro added

---
+++ vclass.java
@@ -0,0 +1,27 @@
+package com.var;
+
+public class vclass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("automation testing");
+		
+		int m=589;
+		int mark[]= {97,99,98,100,99,96};
+		//index ------0, 1, 2, 3,  4, 5
+		System.out.println(mark [3]);
+		
+		char name[] = {'s','a','g','i','e'};
+	
+		System.out.println(name [3]);
+		
+		String edu="sanju went study computer tech";
+		System.out.println(edu);
+		
+		String business ="im going to start it company";
+		System.out.println(business);
+		
+			
+		}
+	}
+
